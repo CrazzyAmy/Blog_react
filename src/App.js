1ee886977e478627f1545b05adde6738f7609c27
CrazzyAmy <amn82910@gmail.com>
2024-03-19 17:36:18 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Desktop from "./Desktop.jsx";
+import './App.css';
+import './all.css';
+import { render } from "@testing-library/react";
+import { useForm } from "react-hook-form";
+import { useEffect , useState} from 'react';
+import { useTransition, animated, useSpring } from '@react-spring/web'
+import {
+    BrowserRouter, 
+    HashRouter,
+    NavLink,
+    Link,
+    Route,
+    Routes,
+    useNavigate,
+    useParams,
+    Outlet
+  } from 'react-router-dom';
+// import { clear } from '@testing-library/user-event/dist/clear';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+
+function App() {
+  const [CartArray, setCartArray] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    mode: "onBlur"
+  });
+  const [token, setToken] = useState(null);
+  
+  return (
+    <div className="app">
+        <AuthProvider value={{ token, setToken }}>
+          <CartContext.Provider value={{CartArray, setCartArray, cartItems,setCartItems}}>
+            <Routes>
+            
+                <Route path="/" element={<Desktop />} />
+            </Routes>
+          </CartContext.Provider>
+        </AuthProvider>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer" type="module"></script>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.5.8/swiper-bundle.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer" type="module"></script>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer" type="module"></script>
+       <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"  type="module"></script>
+       <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Desktop.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { useEffect , useState} from 'react';
+import "./DesktopStyle.css";
+import {Link, Route, Routes, useNavigate} from 'react-router-dom'
+import MyComponent from './App';
+import $ from 'jquery';
+import { clear } from '@testing-library/user-event/dist/clear';
+import CartContext from './CartContext.js';
+import { useAuth } from "./Context.js";
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const url = 'http://localhost:3000';
+window.$ = $;
+function myfunction(){
+//   document.body.addEventListener('touchmove', function(e){
+//   if(!$('div').hasClass('scroll')){
+//      e.preventDefault();
+//   }
+// },{ passive: false })
+$('.imf').click(function () {$('body').animate({scrollTop: 0}, 1500)});
+}
+
+
+
+function Desktop(){
+  
+  const MySwal = withReactContent(Swal);
+  // const url = 'https://todoo.5xcamp.us/';
+  const { token, setToken } = useAuth();
+  const navigate = useNavigate();
+  const [todos, setTodos]= useState([]);
+  const [value,setValue] = useState(""); // todo input
+  const [tabState,setTabState] = useState("all"); // tab state
+
+  const headers = {
+    Authorization: token
+  }
+  
+  useEffect(()=>{
+    getTodo();
+  },[]);
+
+  function getTodo(){
+    console.log('重新取得todo')
+    axios.get(url, {headers}).then((res)=>{
+      setTodos(res.data.todos);
+    }).catch(err=>{
+      return MySwal.fire({
+        icon: 'error',
+        title: '取得失敗',
+      })
+    })
+  }
+  const logout = (e) => {
+    e.preventDefault();
+  
+    axios.delete(url, {headers}).then(res=>{
+      setToken(null);
+      navigate('/login');
+    }).catch(err=>{
+      setToken(null);
+      navigate('/login');
+    })
+  }
+  const loginoutRender = () => {
+    // todo 有值
+    // if(todos.length){
+      let todolist = [];
+      console.log(token);
+      if (token) {
+        // 全部
+        return (todolist = <Link to={"/login"} className="a1">登出</Link> )
+        // todolist = todos.map((item, i)=>{
+        //   return <Link to={"/login"} className="a1">登出</Link> 
+        // })
+      } else {
+          return  (todolist = <Link to={"/login"} className="a1">登入</Link> )
+      }
+
+  }
+  const nicknameRender = () => {
+    // todo 有值
+    // if(todos.length){
+      let todolist = [];
+      console.log(token);
+      if (token) {
+        // 全部
+        return (todolist = <Link to={"/login"} className="a1">登出</Link> )
+      } else {
+          return  (todolist = <Link to={"/signup"} className="a1">註冊</Link> )
+      }
+  }
+  return (
+    
+    <div className="desktop">{/*navbar+banner+group234+top */}
+      <div className="div">{/*navbar+banner+group234 */}
+        
+        
+        
+      </div>
+      <div class="top">
+        <a href="#" onClick={myfunction()}>
+          <img src={top} alt="" class="imf" width="50px"/>
+        </a>
+      </div>
+      
+      
+    </div>
+  );
+};
+
+export default Desktop;
